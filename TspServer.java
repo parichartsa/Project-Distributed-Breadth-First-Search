7dbc7f449b3b79d7b9f91e137f0213ffe72df3e9
parichart <89275029+parichartsa@users.noreply.github.com>
2023-05-26 22:39:48 +0700

    Update TspServer.java

--- TspServer.java
+++ TspServer.java
@@ -8,17 +8,12 @@ import java.net.Socket;
 public class TspServer {
     public static void main(String[] args) {
         try {
-            // สร้าง ServerSocket ให้เฝ้ารอการเชื่อมต่อจาก Client ที่ Port 5000
             ServerSocket serverSocket = new ServerSocket(5000);
-            
             System.out.println("Server started. Waiting for clients...");
 
-            // รอการเชื่อมต่อจาก Client
             Socket clientSocket = serverSocket.accept();
-
             System.out.println("Client connected.");
 
-            // สร้าง Reader และ Writer เพื่ออ่านข้อมูลเข้าและส่งข้อมูลไปยัง Client
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
@@ -26,12 +21,10 @@ public class TspServer {
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Received: " + inputLine);
 
-                // ทำการแก้ปัญหา TSP และส่งผลลัพธ์กลับไปยัง Client
                 String result = solveTSP(inputLine);
                 out.println(result);
             }
 
-            // ปิดการเชื่อมต่อ
             in.close();
             out.close();
             clientSocket.close();
@@ -41,12 +34,110 @@ public class TspServer {
         }
     }
 
-    // ฟังก์ชันแก้ปัญหา TSP
     private static String solveTSP(String input) {
-        // โค้ดการแก้ปัญหา TSP ขนาดไม่เกิน 4 เมือง
-        // ...
+        String[] cities = input.split(","); // Split city names into an array
 
-        // ส่งผลลัพธ์กลับในรูปแบบ String
-        return "TSP solution";
+        int numCities = cities.length; // Number of cities
+        int[] permutation = new int[numCities]; // Store the permutation of city visits
+        int[] bestPermutation = new int[numCities]; // Store the best permutation with the shortest distance
+        double shortestDistance = Double.POSITIVE_INFINITY; // Store the shortest distance
+
+        // Create an initial permutation [0, 1, 2, ..., numCities-1]
+        for (int i = 0; i < numCities; i++) {
+            permutation[i] = i;
+        }
+
+        // Check all possible routes
+        do {
+            double distance = calculateDistance(permutation, cities); // Calculate the distance for the current permutation
+
+            // Store the shortest distance and permutation
+            if (distance < shortestDistance) {
+                shortestDistance = distance;
+                System.arraycopy(permutation, 0, bestPermutation, 0, numCities);
+            }
+        } while (nextPermutation(permutation)); // Generate the next permutation
+
+        // Create the path for the best permutation
+        StringBuilder pathBuilder = new StringBuilder();
+        for (int i = 0; i < numCities; i++) {
+            int cityIndex = bestPermutation[i];
+            pathBuilder.append(cities[cityIndex]);
+            if (i < numCities - 1) {
+                pathBuilder.append(" -> ");
+            }
+        }
+
+        String shortestPath = pathBuilder.toString();
+
+        return "Shortest Path: " + shortestPath + "\nShortest Distance: " + shortestDistance;
+    }
+
+    private static double calculateDistance(int[] permutation, String[] cities) {
+        double distance = 0;
+        int numCities = permutation.length;
+
+        for (int i = 0; i < numCities - 1; i++) {
+            int cityIndex1 = permutation[i];
+            int cityIndex2 = permutation[i + 1];
+            String city1 = cities[cityIndex1];
+            String city2 = cities[cityIndex2];
+
+            distance += calculateDistanceBetweenCities(city1, city2);
+        }
+
+        return distance;
+    }
+
+    private static double calculateDistanceBetweenCities(String city1, String city2) {
+        // Distance matrix between cities
+        double[][] distanceMatrix = {
+            {0, 10, 15, 20},
+            {10, 0, 35, 25},
+            {15, 35, 0, 30},
+            {20, 25, 30, 0}
+        };
+
+        // Assuming city1 and city2 are valid city indices
+        int index1 = Integer.parseInt(city1);
+        int index2 = Integer.parseInt(city2);
+
+        return distanceMatrix[index1][index2];
+    }
+
+    private static boolean nextPermutation(int[] permutation) {
+        int i = permutation.length - 2;
+
+        while (i >= 0 && permutation[i] > permutation[i + 1]) {
+            i--;
+        }
+
+        if (i < 0) {
+            return false; // Last permutation reached
+        }
+
+        int j = permutation.length - 1;
+        while (permutation[j] < permutation[i]) {
+            j--;
+        }
+
+        swap(permutation, i, j);
+
+        int left = i + 1;
+        int right = permutation.length - 1;
+
+        while (left < right) {
+            swap(permutation, left, right);
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
