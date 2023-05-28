a9ac6542db0201699d5f13efef052ca9ed898a6a
parichart <89275029+parichartsa@users.noreply.github.com>
2023-05-28 18:17:36 +0700

    Update TspClient.java

--- TspClient.java
+++ TspClient.java
@@ -7,24 +7,19 @@ import java.net.Socket;
 public class TspClient {
     public static void main(String[] args) {
         try {
-            // connect to Server at IP Address and Port that require
-            Socket socket = new Socket("192.168.1.62", 5000);
+            String serverIP = "192.168.1.180";
+            int serverPort = 5500;
 
-
-            // create Reader and Writer for read data and connect to Server
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Socket socket = new Socket(serverIP, serverPort);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // sent data input to server
-            String input = "TSP input";
-            out.println(input);
-            System.out.println("Sent: " + input);
+            String cities = "City A,City B,City C,City D"; // ตัวอย่างเมืองที่ส่งไปคำนวณ
+            out.println(cities);
 
-            // wait result from server
             String response = in.readLine();
             System.out.println("Received: " + response);
 
-            // close the connection
             in.close();
             out.close();
             socket.close();
